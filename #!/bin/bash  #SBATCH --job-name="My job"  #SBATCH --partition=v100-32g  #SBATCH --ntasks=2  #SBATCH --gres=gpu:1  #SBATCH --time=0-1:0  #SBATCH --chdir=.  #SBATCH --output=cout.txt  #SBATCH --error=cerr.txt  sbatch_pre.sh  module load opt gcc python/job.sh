45c40f1755a9ab3e876bbdaa93cbd336ac8393e9
roy0428 <98080372+roy0428@users.noreply.github.com>
2022-10-29 20:06:03 +0800

    Create job.sh

---
+++ #!/bin/bash  #SBATCH --job-name="My job"  #SBATCH --partition=v100-32g  #SBATCH --ntasks=2  #SBATCH --gres=gpu:1  #SBATCH --time=0-1:0  #SBATCH --chdir=.  #SBATCH --output=cout.txt  #SBATCH --error=cerr.txt  sbatch_pre.sh  module load opt gcc python/job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --job-name="My job"
+#SBATCH --partition=v100-32g
+#SBATCH --ntasks=2
+#SBATCH --gres=gpu:1
+#SBATCH --time=0-1:0
+#SBATCH --chdir=.
+#SBATCH --output=cout.txt
+#SBATCH --error=cerr.txt
+
+module load python/3.9.13-gpu
+localization.sh
